be829ba8532c4d324e1fe36086652bd233cb81e3
zhdanov-ek <zhdanov.ek@gmail.com>
2020-07-01 22:11:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/translater/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.translater.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.translater.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
